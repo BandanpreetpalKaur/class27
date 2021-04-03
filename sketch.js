ff2744ed5da802f6beea90458f9d3d6c98b2b8e1
bandan <anhadbani@gmail.com>
2021-04-03 12:15:10 -0400

    added sligshot.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint=Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
@@ -35,6 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    chain=new Slingshot (bird.body,log5.body)
 
 }
 
@@ -61,4 +62,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    chain.display();
 }
